2716b2b3f7cfe92cd98669dd31dd03f753425241
saiprasathB <saiprasath2805@gmail.com>
2025-04-01 14:14:15 +0530

    summa

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -4,10 +4,10 @@ const User = require("../models/User");
 
 
 router.post("/signup", async (req, res) => {
-    console.log(req.body); // Log the incoming request body for debugging
+    console.log(req.body); 
     const { name, email, phone, password, confirmPassword } = req.body;
   
-    // Validate passwords match
+    
     if (password !== confirmPassword) {
       return res.status(400).json({ message: "Passwords do not match" });
     }
@@ -19,17 +19,17 @@ router.post("/signup", async (req, res) => {
         return res.status(200).json({ message: "Email is already registered" });
       }
   
-      // Create a new user
-      const newUser = new User({ name, email, phone, password, confirmPassword });
+    
+      const newUser = new User({ name, email, phone, password, confirmPassword ,isVerified: false});
   
-      // Save the new user to the database
+      
       await newUser.save();
   
-      // Respond with a success message
+     
       return res.status(201).json({ message: "User created successfully, awaiting admin approval" });
   
     } catch (err) {
-      console.error("Error during signup:", err); // Log the error to the console for debugging
+      console.error("Error during signup:", err); 
       return res.status(500).json({ message: "Error signing up user", error: err });
     }
   });
--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   password: { type: String, required: true },
   confirmPassword: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User-node_practice", userSchema);
